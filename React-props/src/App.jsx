79d87328e4edb57919d9e04521b9dfb4f1b03fb7
Aditya1-Sharma <pekodora6@gmail.com>
2024-03-06 11:47:38 +0530

    React Props and destructuring

---
+++ React-props/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import Card from './Component/Card'
+function App() {
+const price={
+  about: "laptop",
+  name: "macBook"
+}
+  return (
+    <>
+     <h1 className=" text-gray-400 text-4xl font-bold underline text-center">
+      Hello world!
+    </h1>
+    
+    <Card name="macbook" obj = {price}/>
+    <Card name="Dell inspiron" obj = {price}/>
+
+
+    </>
+  )
+}
+
+export default App
