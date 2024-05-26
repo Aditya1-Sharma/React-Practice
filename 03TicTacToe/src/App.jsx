88a85a543c412cc113df6d78d8d812c0e804c55e
Aditya1-Sharma <pekodora6@gmail.com>
2024-05-27 02:58:28 +0530

    Tic Tac Toe Game

---
+++ 03TicTacToe/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Board from "./Component/Board";
+import Game from "./Component/Game";
+
+function App() {
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Game />
+    </>
+  );
+}
+
+export default App;
